93281cfa947b2ca1e782f06506c65fe5a2cc2787
WDRETZER <walter_retzer@yahoo.com.br>
2021-12-01 13:49:32 -0300

    Implementação da Tela de Transferência do Aplicativo Wallet

--- app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
+++ app/src/main/java/com/digitalhouse/dhwallet/MainActivity.kt
@@ -2,10 +2,16 @@ package com.digitalhouse.dhwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentContainer: FragmentContainerView by lazy {
+        findViewById(R.id.fragment_container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.transfer_fragment)
     }
 }
---
+++ app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
@@ -0,0 +1,8 @@
+package com.digitalhouse.dhwallet
+
+import androidx.fragment.app.Fragment
+
+class TransferFragment: Fragment(R.layout.transfer_fragment) {
+
+}
+
